512e4c6ad2f0e74d3cbd0b5e39c641b0e722600a
aaleksander <aleksandranufriev@yandex.ru>
2014-01-23 23:43:54 +0400

    Добавил F

--- tmp.hs
+++ tmp.hs
@@ -12,7 +12,7 @@ y val = (Y, val)
 z :: Double -> (Dir, Double)
 z val = (Z, val)
 
-data Command = Command Code [(Dir, Double)] 
+data Command = Command Code [(Dir, Double)]
 				  
 
 --конвертим пару "направление-значение" в строку
@@ -28,6 +28,7 @@ g2Str :: Command -> String
 g2Str d 
 	| Command G0 l <- d = "G0 " ++ l2Str l ++ "\n"
 	| Command G1 l <- d = "G1 " ++ l2Str l ++ "\n"
+	| Command F [(_, val)] <- d = "F " ++ show val ++ "\n"
 	where l2Str ll= 
 		foldr (++) " " $ map _2Str ll
 
@@ -38,13 +39,14 @@ g1 :: [(Dir, Double)] -> Command
 g1 list = Command G1 list
 
 f :: Double -> Command
-f val = Command F val
+f val  = Command F [(X, val)]
 
 path = [
-	f 300
+	f 300,
 	g0 [x 0, y 0, z 0],
 	g1 [x 10.0, y 20]
 	]
+
 
 ps :: String -> IO()
 ps s = do
